011b361773bc2b47d89df4ddd506bac38bd9598d
João Pacheco <jao.pacheco1@gmail.com>
2025-06-30 17:30:25 -0300

    feat: implement reset endpoint

--- app/controllers/eventController.js
+++ app/controllers/eventController.js
@@ -1,4 +1,5 @@
 const service = require('../services/accountService');
+const store = require('../models/memoryStore');
 
 exports.handleEvent = (req, res) => {
   const result = service.processEvent(req.body);
@@ -11,4 +12,9 @@ exports.getBalance = (req, res) => {
   const balance = service.getBalance(account_id);
   if (balance === null) return res.status(404).send(0);
   return res.status(200).send(balance.toString());
+};
+
+exports.reset = (req, res) => {
+  store.reset();
+  res.sendStatus(200);
 };
--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,6 @@ const controller = require('./controllers/eventController');
 
 router.get('/balance', controller.getBalance);
 router.post('/event', controller.handleEvent);
+router.post('/reset', controller.reset); 
 
 module.exports = router;

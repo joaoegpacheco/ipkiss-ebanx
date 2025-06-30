267c101523b520aef0c98901613f734e4ddc53ce
João Pacheco <jao.pacheco1@gmail.com>
2025-06-30 16:02:29 -0300

    feat: event controller added to API from account service

---
+++ app/controllers/eventController.js
@@ -0,0 +1,14 @@
+const service = require('../services/accountService');
+
+exports.handleEvent = (req, res) => {
+  const result = service.processEvent(req.body);
+  if (!result) return res.status(404).send(0);
+  return res.status(201).json(result);
+};
+
+exports.getBalance = (req, res) => {
+  const { account_id } = req.query;
+  const balance = service.getBalance(account_id);
+  if (balance === null) return res.status(404).send(0);
+  return res.status(200).send(balance.toString());
+};
